b67377a6d89e200353aa65c5444ea40d13174876
SShyamKumar <shyamng48@gmail.com>
2022-09-30 08:03:10 +0530

    Blog express notes added

--- index.js
+++ index.js
@@ -3,9 +3,22 @@ const app = express();
 const path = require("path");
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+
+// const harryMiddleware = (req, res, next) =>{
+//   console.log(req)
+//   next()
+// }
+//own middleware, req, res, next.
+
+app.use(express.static(path.join(__dirname, "public")))
+// app.use(harryMiddleware)
+//middleware, keep access of request and response
+
+app.get("/hello/:name", (req, res) => {
+  res.send("Hello World! " + req.params.name);
 });
+//want to print name along with hello path, also print that name, then use this
+//req.params.name put that code to use this functionality
 
 app.get("/about", (req, res) => {
   //   res.send('about')
